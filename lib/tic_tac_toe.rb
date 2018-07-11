8a933ec790a9e56eae6ae25b807167fef4f6fe23
Marcela Fontes Lima Guerra <marcela.guerra@gmail.com>
2018-07-11 13:15:17 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -66,6 +66,10 @@ def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
 
+def turn_count(board)
+
+end
+
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
